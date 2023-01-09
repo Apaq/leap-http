67831e6e2ab7ab8fbb75bb1ca746e0e5263eb52b
Michael Krog <mic@apaq.dk>
2023-01-09 12:26:32 +0100

    Adds a bit more logging

--- src/http/converter/json-message-converter.ts
+++ src/http/converter/json-message-converter.ts
@@ -32,6 +32,7 @@ export class JsonMessageConverter implements HttpMessageConverter {
     }
 
     write<T>(data: T): Promise<{ headers: Headers, body: BodyInit }> {
+        console.log('Writing object to JSON: ', data);
         PropertyMapper.mapValues(this.dateToStringConverter, data);
 
         const headers = new Headers();
