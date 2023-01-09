2fa4fccd0e6b4f8eac20b8f43f4bb4e62c4ffa2c
Michael Krog <mic@apaq.dk>
2023-01-09 12:33:07 +0100

    Ads event more logging

--- src/http/converter/property-mapper.ts
+++ src/http/converter/property-mapper.ts
@@ -3,11 +3,14 @@ import { ValueConverter } from "./value-converter";
 export class PropertyMapper {
 
     public static mapValues(converter: ValueConverter<any, any>, object: Object) {
+        console.log('Mapping object: ', object, converter);
         if (!object || !(object instanceof Object)) {
+            console.log('Object not considered an object: ', object, typeof(object));
             return;
         }
 
         if (object instanceof Array) {
+            console.log('Object considered an array: ', object, typeof(object));
             for (const item of object) {
                 this.mapValues(converter, item);
             }
@@ -15,6 +18,8 @@ export class PropertyMapper {
 
         for (const key of Object.keys(object)) {
             const value = object[key];
+
+            console.log('Mapping value: ', key,  value, typeof(value));
             
             if (value instanceof Array) {
                 for (const item of value) {
